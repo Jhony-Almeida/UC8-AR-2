9d71f28f430bc255984cc549bdaa68d391fe86f5
Jhony Almeida <jhony12358oficial@gmail.com>
2022-07-06 12:00:19 -0300

    Ativade Encontro Remoto 2

---
+++ listaDeAlunos.js
@@ -0,0 +1,16 @@
+let numeroDeAlunos = 10;
+
+for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+    console.log(contador);
+       
+
+    if (contador == 0) {
+        console.log("Número Igual a Zero")
+    }
+    else if (contador % 2 == 0) {
+        console.log("número par")
+    }
+    else {
+        console.log("número impar")
+    }
+}
